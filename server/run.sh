a4b2447f1297d6de382030203a62311ad9eb6b97
NexusLee <tonexus.lee@gmail.com>
2017-10-12 14:10:12 +0800

    note api

--- server/run.sh
+++ server/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm -p 8000:3000 -v ~/work/angular/my-app/server:/go/src/go-restful -w /go/src/go-restful notes
+docker run -it --rm -p 8000:3000 -v ~/work/angular/ng-app/server:/go/src/go-restful -w /go/src/go-restful notes
