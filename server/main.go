2eeeff0c4a93c0a9dc380e3e32b1617b32934e69
NexusLee <tonexus.lee@gmail.com>
2017-10-18 10:07:32 +0800

    instance note func

--- server/main.go
+++ server/main.go
@@ -13,6 +13,7 @@ func main(){
 
   restful.Add(api.NewUserService())
 //  restful.Add(api.NewNoteService())
+//  api.NoteResource{}
   restful.Add(api.NoteResource{}.NewNoteService())
   log.Print("start listening on localhost:3000")
   log.Fatal(http.ListenAndServe(":3000", nil))
